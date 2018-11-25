3bc1454c1622209f679240c2d5a247a491bb7b72
thinceller <kohei.ct@icloud.com>
2018-11-26 07:47:47 +0900

    サーバー操作処理実装

--- src/services/AwsService.js
+++ src/services/AwsService.js
@@ -16,7 +16,7 @@ class AwsService {
    * Lambda関数を実行
    * @param {string} functionName - Lambda関数名
    * @param {Object} payload - Lambda関数に渡すパラメータ
-   * @return {Object} 関数実行結果を返す
+   * @return {Promise} 関数実行結果を返す
    */
   invokeLambda = (functionName, payload) => {
     const params = {
@@ -26,10 +26,24 @@ class AwsService {
     return this.lambda.invoke(params).promise();
   };
 
+  /**
+   * サーバーの状態を取得
+   * @return {Promise} レスポンス
+   */
   getServerStatus = () => {
     const payload = { action: 'status' }
     return this.invokeLambda(this.lambdaName, payload)
-  }
+  };
+
+  /**
+   * 引数のアクションに応じてサーバーを操作
+   * @param {string} action - サーバーの起動or停止 'start'or'stop'
+   * @return {Promise} レスポンス
+   */
+  operateServer = action => {
+    const payload = { action: action };
+    return this.invokeLambda(this.lambdaName, payload);
+  };
 }
 
 const awsService = new AwsService();
